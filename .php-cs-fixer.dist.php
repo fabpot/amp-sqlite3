a4d92578ea55b3e7fed3aedc792017032787ccde
Fabien Potencier <fabien@potencier.org>
2026-07-14 14:12:11 +0200

    Add license header to all PHP files, enforced by php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,23 @@
 <?php
 
-$config = new Amp\CodeStyle\Config();
+$config = new class extends Amp\CodeStyle\Config {
+    public function getRules(): array
+    {
+        return [
+            ...parent::getRules(),
+            'header_comment' => [
+                'header' => <<<'EOF'
+                    This file is part of the fabpot/amphp-sqlite3 package.
+
+                    (c) Fabien Potencier <fabien@potencier.org>
+
+                    For the full copyright and license information, please view the LICENSE
+                    file that was distributed with this source code.
+                    EOF,
+            ],
+        ];
+    }
+};
 $config->getFinder()
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/test');
--- src/SqliteJournalMode.php
+++ src/SqliteJournalMode.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the fabpot/amphp-sqlite3 package.
+ *
+ * (c) Fabien Potencier <fabien@potencier.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Fabpot\Amp\Sqlite;
 
 enum SqliteJournalMode: string
